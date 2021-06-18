d7a433185e6447bf08bb0387e0691f0b64d9db69
cmur97 <63924852+cmur97@users.noreply.github.com>
2021-06-19 02:08:04 +0900

    Update EarningsCalculator.py

--- EarningsCalculator.py
+++ EarningsCalculator.py
@@ -1,20 +1,14 @@
-
-
-
 def earnings_calculator():
     initial_investment = int(input("Enter initial investment: "))
     num_trades = int(input("Enter the number of trades: "))
     percent_returns = float(input("Enter the percent return for each trade: "))
     earnings_per_trade = initial_investment * percent_returns
     print(f"You will earn ${earnings_per_trade} per trade")
-    total = initial_investment # 5000
-
+    total = initial_investment
+    
     for num in range(1, num_trades + 1):
         initial_investment += earnings_per_trade
-
         print(f"Trade {num}: {initial_investment}")
         print(f"Total earnings for trade {num}: {(initial_investment - total)}")
-
-
 
 earnings_calculator()
